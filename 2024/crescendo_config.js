53ff3ae9b171c3777ab4ac7ef074f52b6dfb7e6d
Espeii <alexbuckley161@gmail.com>
2024-03-15 11:34:36 -0400

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -54,8 +54,16 @@ var config_data = `
       "type": "team",
       "min": 1,
       "max": 99999
-    }
-    
+    },
+    { "name": "Auto Start Position",
+       "code": "as",
+       "type": "clickable_image",
+       "filename": "2024/field_image.png",
+       "clickRestriction": "one",
+       "allowableResponses": "1 12 13 24 25 36 37 48 49 60 61 72",
+       "shape": "circle 5 black red true"
+       "required":"false"
+     }
   ],
   "auton": [
     { "name": "Leave Starting Zone",
@@ -185,11 +193,4 @@ var config_data = `
   ]
 }`;
 
-// { "name": "Auto Start Position",
-//       "code": "as",
-//       "type": "clickable_image",
-//       "filename": "2024/field_image.png",
-//       "clickRestriction": "one",
-//       "allowableResponses": "1 12 13 24 25 36 37 48 49 60 61 72",
-//       "shape": "circle 5 black red true"
-//     }
+// 
