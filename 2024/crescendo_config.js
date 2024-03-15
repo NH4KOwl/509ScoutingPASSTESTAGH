c7684d5bde517f2a272bdbda1c10c69ef43c94a6
Espeii <alexbuckley161@gmail.com>
2024-03-15 09:23:43 -0400

    event code

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "", 
+      "defaultValue": "reading", 
       "required": "false"
     },
     { "name": "Match Level",
