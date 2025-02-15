45ac0cdac5e6207f59156872e28daec925c38464
NH4KOwl <95386108+NH4KOwl@users.noreply.github.com>
2025-02-14 21:40:17 -0500

    Update reefscape_pit_config.js

--- 2025/reefscape_pit_config.js
+++ 2025/reefscape_pit_config.js
@@ -4,34 +4,41 @@ var config_data = `
   "page_title": "Reefscape",
   "pitConfig": "true",
   "prematch": [
-    {"name": "Scouter Initials", 
+    {
+      "name": "Scouter Initials",
       "code": "si",
       "type": "text"
-      },
-    { "name": "Team #",
+    },
+    {
+      "name": "Team #",
       "code": "tn",
       "type": "number"
     },
-    { "name": "Width",
+    {
+      "name": "Width",
       "code": "wid",
       "type": "number",
       "defaultValue": "0"
     },
-    {"name": "Length", 
-     "code": "le", 
-     "type": "number", 
-     "defaultValue": "0"
+    {
+      "name": "Length",
+      "code": "le",
+      "type": "number",
+      "defaultValue": "0"
     },
-    { "name": "Weight",
+    {
+      "name": "Weight",
       "code": "wei",
       "type": "number",
       "defaultValue": "0"
     },
-    {"name": "Swerve Ratio", 
+    {
+      "name": "Swerve Ratio",
       "code": "sr",
       "type": "text"
     },
-    { "name": "Drivetrain",
+    {
+      "name": "Drivetrain",
       "code": "drv",
       "type": "radio",
       "choices": {
@@ -43,107 +50,133 @@ var config_data = `
       },
       "defaultValue": "o"
     },
-    { "name": "Other Drivetrain",
+    {
+      "name": "Other Drivetrain",
       "code": "odt",
       "type": "text",
       "size": 20,
       "maxSize": 50
     },
-    {"name": "April tag alignment?",
-     "code": "apr", 
-     "type": "bool"
+    {
+      "name": "April tag alignment?",
+      "code": "apr",
+      "type": "bool"
     },
-    {"name": "Can score: ", 
-     "code": "csa", 
-     "type": "radio",
-     "choices": {
-        "a": "L1<br>", 
-        "aa": "Can not<br>"}
-     },
-     {"name": "Can score: ", 
-     "code": "csb", 
-     "type": "radio",
-     "choices": {
-        "b": "L2<br>",
-        "ab": "Can not<br>"} 
-      },
-        {"name": "Can score: ", 
-     "code": "csc", 
-     "type": "radio",
-     "choices": {
-        "c": "L3<br>", 
-        "ac": "Can not<br>"}
-      },
-        {"name": "Can score: ", 
-     "code": "csd", 
-     "type": "radio",
-     "choices": {
-        "d": "L4<br>", 
-        "ad": "Can not<br>"}
-      },
-        {"name": "Can score: ", 
-     "code": "cse", 
-     "type": "radio",
-     "choices": {
-        "e": "Barge (from ground)<br>", 
-        "ae": "Can not<br>"}
-      },
-        {"name": "Can score: ", 
-     "code": "csf", 
-     "type": "radio",
-     "choices": {
-        "f": "Processor<br>",
-        "af": "Can not<br>"} 
-    },
-    {"name": "Can climb: ", 
-      "code": "cli", 
+    {
+      "name": "Can score: ",
+      "code": "csa",
       "type": "radio",
       "choices": {
-        "n": "Deep Cage<br>", 
-        "f": "Shallow<br>", 
-        "c": "Both<br>", 
+        "a": "L1<br>",
+        "aa": "Can not<br>"
+      }
+    },
+    {
+      "name": "Can score: ",
+      "code": "csb",
+      "type": "radio",
+      "choices": {
+        "b": "L2<br>",
+        "ab": "Can not<br>"
+      }
+    },
+    {
+      "name": "Can score: ",
+      "code": "csc",
+      "type": "radio",
+      "choices": {
+        "c": "L3<br>",
+        "ac": "Can not<br>"
+      }
+    },
+    {
+      "name": "Can score: ",
+      "code": "csd",
+      "type": "radio",
+      "choices": {
+        "d": "L4<br>",
+        "ad": "Can not<br>"
+      }
+    },
+    {
+      "name": "Can score: ",
+      "code": "cse",
+      "type": "radio",
+      "choices": {
+        "e": "Barge (from ground)<br>",
+        "ae": "Can not<br>"
+      }
+    },
+    {
+      "name": "Can score: ",
+      "code": "csf",
+      "type": "radio",
+      "choices": {
+        "f": "Processor<br>",
+        "af": "Can not<br>"
+      }
+    },
+    {
+      "name": "Can climb: ",
+      "code": "cli",
+      "type": "radio",
+      "choices": {
+        "n": "Deep Cage<br>",
+        "f": "Shallow<br>",
+        "c": "Both<br>",
         "b": "Neither<br>",
         "k": "Not Scouted"
       },
       "defaultValue": "k"
     },
-    {"name": "Can Pick Up: ", 
-      "code": "cpc", 
+    {
+      "name": "Can Pick Up: ",
+      "code": "cpc",
       "type": "radio",
       "choices": {
-       "f": "Floor Coral<br>",
-       "af": "Can not<br>" }
+        "f": "Floor Coral<br>",
+        "af": "Can not<br>"
+      }
     },
-    {"name": "Can Pick Up: ", 
-      "code": "cph", 
+    {
+      "name": "Can Pick Up: ",
+      "code": "cph",
       "type": "radio",
       "choices": {
-      "f": "Human Player Coral<br>",
-      "af": "Can not<br>" }
+        "f": "Human Player Coral<br>",
+        "af": "Can not<br>"
+      }
     },
-    {"name": "Can Pick Up: ", 
-      "code": "cpr", 
+    {
+      "name": "Can Pick Up: ",
+      "code": "cpr",
       "type": "radio",
       "choices": {
-      "f": "Reef Algae<br>",
-      "af": "Can not<br>"}
+        "f": "Reef Algae<br>",
+        "af": "Can not<br>"
+      }
     },
-    {"name": "Can Pick Up: ", 
-      "code": "cpa", 
+    {
+      "name": "Can Pick Up: ",
+      "code": "cpa",
       "type": "radio",
       "choices": {
-      "f": "Floor Algae<br>",
-      "af": "Can not<br>"}
+        "f": "Floor Algae<br>",
+        "af": "Can not<br>"
+      }
     },
-    { "name": "# of Auto Coral",
+    {
+      "name": "# of Auto Coral",
       "code": "auc",
       "type": "counter"
     },
-    { "name": "# of Auto Algae",
+    {
+      "name": "# of Auto Algae",
       "code": "aua",
       "type": "counter"
     },
-    { "name": "Autos Descriptions",
+    {
+      "name": "Autos Descriptions",
       "code": "aut",
       "type": "text",
       "size": 20,
@@ -164,13 +197,10 @@ var config_data = `
       "maxSize": 300
     }
   ],
-  "auton": [
-  ],
-  "teleop": [
-  ],
-  "endgame": [
-  ],
-  "postmatch": [
-  ]
-}`;
+  "auton": [],
+  "teleop": [],
+  "endgame": [],
+  "postmatch": []
+}
+`;
 //documentation 
